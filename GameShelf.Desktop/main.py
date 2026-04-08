e54621fda5bdb2dac12a846e0b49d5fc646c067e
Krzysztof Sulkowski <krzysztof.sulkowski@studenci.collegiumwitelona.pl>
2026-04-08 08:17:51 +0200

    Update main.py with first UI mockup, update requirements.txt file with GUI tool (PySide6)

--- GameShelf.Desktop/main.py
+++ GameShelf.Desktop/main.py
@@ -1,28 +1,15 @@
-from services.session import set_token
-from services.auth_service import login, get_user_profile
-from services.collection_service import get_my_collection
+import sys
+from PySide6.QtWidgets import QApplication
+from views.login_view import LoginView
+
 
 def main():
-    email = input("Email: ")
-    password = input("Password: ")
+    app = QApplication(sys.argv)
 
-    token = login(email, password)
+    window = LoginView()
+    window.show()
 
-    if not token:
-        print("Login failed")
-        return
-
-    set_token(token)
-
-    user = get_user_profile()
-    print("USER:")
-    print(user)
-
-    games = get_my_collection()
-
-    print("GAMES:")
-    for game in games:
-        print(game)
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":

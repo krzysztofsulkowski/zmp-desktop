d841183c17a7ecdc4a4b3c6989c0629632076929
Krzysztof Sulkowski <krzysztof.sulkowski@studenci.collegiumwitelona.pl>
2026-04-09 13:24:09 +0200

    Add qss file for styling the landing page, modified the landing page to better mirror the sketch

--- GameShelf.Desktop/views/landing_view.py
+++ GameShelf.Desktop/views/landing_view.py
@@ -1,6 +1,6 @@
 from PySide6.QtWidgets import (
     QWidget, QVBoxLayout, QHBoxLayout,
-    QPushButton, QLabel
+    QPushButton, QLabel, QFrame
 )
 from PySide6.QtCore import Qt
 
@@ -11,61 +11,93 @@ class LandingView(QWidget):
 
         self.controller = controller
         self.setWindowTitle("GameShelf")
+        self.setMinimumSize(1440, 840)
+
+        self.load_stylesheet()
+
+        outer_layout = QVBoxLayout()
+        outer_layout.setContentsMargins(30, 30, 30, 30)
+
+        main_frame = QFrame()
+        main_frame.setObjectName("landingFrame")
 
         main_layout = QVBoxLayout()
+        main_layout.setContentsMargins(45, 35, 45, 35)
+        main_layout.setSpacing(0)
 
         top_bar = QHBoxLayout()
+        top_bar.setContentsMargins(0, 0, 0, 0)
         top_bar.addStretch()
 
-        self.login_button = QPushButton("Login")
-        self.register_button = QPushButton("Register")
+        self.login_button = QPushButton("LOGOWANIE")
+        self.login_button.setFixedSize(150, 45)
+
+        self.register_button = QPushButton("REJESTRACJA")
+        self.register_button.setFixedSize(175, 47)
+        self.register_button.setObjectName("primaryButton")
 
         top_bar.addWidget(self.login_button)
+        top_bar.addSpacing(20)
         top_bar.addWidget(self.register_button)
 
         content_layout = QHBoxLayout()
+        content_layout.setContentsMargins(0, 40, 0, 0)
+        content_layout.setSpacing(70)
 
         left_side = QVBoxLayout()
-        right_side = QVBoxLayout()
+        left_side.setSpacing(18)
+        left_side.setAlignment(Qt.AlignCenter)
 
         self.logo_label = QLabel("GAME SHELF LOGO")
         self.logo_label.setAlignment(Qt.AlignCenter)
+        self.logo_label.setObjectName("landingLogo")
+        self.logo_label.setFixedHeight(120)
 
         self.headline_label = QLabel(
             "Twoje gry w jednym miejscu. I ludzie,\nktórzy grają w to samo."
         )
         self.headline_label.setAlignment(Qt.AlignCenter)
+        self.headline_label.setObjectName("landingHeadline")
+        self.headline_label.setFixedWidth(584)
 
         self.description_label = QLabel(
             "Uporządkuj gry z różnych platform i sprawdzaj, w co grają Twoi\n"
             "znajomi - w jednym miejscu, bez przełączania między aplikacjami."
         )
         self.description_label.setAlignment(Qt.AlignCenter)
+        self.description_label.setObjectName("landingDescription")
+        self.description_label.setFixedWidth(584)
+        self.description_label.setWordWrap(True)
 
         self.join_button = QPushButton("DOŁĄCZ DO NAS!")
+        self.join_button.setFixedSize(232, 40)
+        self.join_button.setObjectName("primaryButton")
 
         self.small_text_label = QLabel(
             "Zarejestruj się za darmo i rozpocznij tworzenie kolekcji!"
         )
         self.small_text_label.setAlignment(Qt.AlignCenter)
+        self.small_text_label.setObjectName("landingSmallText")
 
         left_side.addStretch()
-        left_side.addWidget(self.logo_label)
-        left_side.addSpacing(20)
-        left_side.addWidget(self.headline_label)
-        left_side.addSpacing(15)
-        left_side.addWidget(self.description_label)
-        left_side.addSpacing(20)
-        left_side.addWidget(self.join_button)
-        left_side.addSpacing(10)
-        left_side.addWidget(self.small_text_label)
+        left_side.addWidget(self.logo_label, alignment=Qt.AlignCenter)
+        left_side.addSpacing(35)
+        left_side.addWidget(self.headline_label, alignment=Qt.AlignCenter)
+        left_side.addWidget(self.description_label, alignment=Qt.AlignCenter)
+        left_side.addSpacing(18)
+        left_side.addWidget(self.join_button, alignment=Qt.AlignCenter)
+        left_side.addWidget(self.small_text_label, alignment=Qt.AlignCenter)
         left_side.addStretch()
 
-        self.preview_placeholder = QLabel("PREVIEW IMAGE PLACEHOLDER")
-        self.preview_placeholder.setAlignment(Qt.AlignCenter)
+        right_side = QVBoxLayout()
+        right_side.setAlignment(Qt.AlignCenter)
+
+        self.preview_placeholder = QFrame()
+        self.preview_placeholder.setObjectName("previewPlaceholder")
+        self.preview_placeholder.setFixedSize(722, 384)
 
         right_side.addStretch()
-        right_side.addWidget(self.preview_placeholder)
+        right_side.addWidget(self.preview_placeholder, alignment=Qt.AlignCenter)
         right_side.addStretch()
 
         content_layout.addLayout(left_side, 1)
@@ -74,8 +106,15 @@ class LandingView(QWidget):
         main_layout.addLayout(top_bar)
         main_layout.addLayout(content_layout)
 
-        self.setLayout(main_layout)
+        main_frame.setLayout(main_layout)
+        outer_layout.addWidget(main_frame)
+
+        self.setLayout(outer_layout)
 
         self.login_button.clicked.connect(self.controller.show_login)
         self.register_button.clicked.connect(self.controller.show_register)
         self.join_button.clicked.connect(self.controller.show_register)
+
+    def load_stylesheet(self):
+        with open("GameShelf.Desktop/styles/landing_view.qss", "r") as file:
+            self.setStyleSheet(file.read())

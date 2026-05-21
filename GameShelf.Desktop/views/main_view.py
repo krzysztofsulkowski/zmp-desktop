917263757a27dbd84991c6f16bee2cd05a3f53d3
Krzysztof Sulkowski <krzysztof.sulkowski@studenci.collegiumwitelona.pl>
2026-05-21 07:38:31 +0200

    main view window filter combo box add arrows

--- GameShelf.Desktop/views/main_view.py
+++ GameShelf.Desktop/views/main_view.py
@@ -54,6 +54,63 @@ from views.move_game_dialog import MoveGameDialog
 from views.rate_game_dialog import RateGameDialog
 from views.chat_view import ChatView
 
+class MainFilterComboBox(QComboBox):
+    def __init__(self, assets_dir):
+        super().__init__()
+
+        self.assets_dir = Path(assets_dir)
+        self.setObjectName("mainFilter")
+        self.set_arrow_icon("ArrowDownIcon.svg")
+
+    def showPopup(self):
+        self.set_arrow_icon("ArrowUpIcon.svg")
+        super().showPopup()
+
+    def hidePopup(self):
+        super().hidePopup()
+        self.set_arrow_icon("ArrowDownIcon.svg")
+
+    def set_arrow_icon(self, icon_name):
+        icon_path = (self.assets_dir / icon_name).as_posix()
+        self.setStyleSheet(f"""
+            QComboBox#mainFilter {{
+                background-color: #261C40;
+                color: #ffffff;
+                border: none;
+                border-radius: 7px;
+                padding-left: 10px;
+                padding-right: 34px;
+                padding-top: 0px;
+                padding-bottom: 0px;
+                font-family: "Figtree Light", "Figtree", "Segoe UI", "Arial";
+                font-size: 16px;
+                font-weight: 300;
+                min-height: 36px;
+                max-height: 36px;
+            }}
+
+            QComboBox#mainFilter::drop-down {{
+                border: none;
+                width: 34px;
+                subcontrol-origin: padding;
+                subcontrol-position: top right;
+            }}
+
+            QComboBox#mainFilter::down-arrow {{
+                image: url({icon_path});
+                width: 12px;
+                height: 12px;
+                margin-right: 10px;
+            }}
+
+            QComboBox#mainFilter QAbstractItemView {{
+                background-color: #21153B;
+                color: #ffffff;
+                border: 1px solid #8B5CF6;
+                selection-background-color: #7C3AED;
+            }}
+        """)
+
 
 class MainView(QWidget):
     PROFILE_VIEW_INDEX = 0
@@ -331,12 +388,11 @@ class MainView(QWidget):
         self.filters_layout.setContentsMargins(0, 0, 0, 0)
         self.filters_layout.setSpacing(14)
 
-        self.genre_filter = QComboBox()
-        self.platform_filter = QComboBox()
-        self.sort_filter = QComboBox()
+        self.genre_filter = MainFilterComboBox(self.base_dir / "assets")
+        self.platform_filter = MainFilterComboBox(self.base_dir / "assets")
+        self.sort_filter = MainFilterComboBox(self.base_dir / "assets")
 
         for combo in [self.genre_filter, self.platform_filter, self.sort_filter]:
-            combo.setObjectName("mainFilter")
             combo.setFixedHeight(36)
 
         self.genre_filter.setFixedWidth(196)
